9c7e35b2b7eba8a2674c8ec8df2ba09166f8a573
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2018-11-16 15:42:05 -0600

    'npm start' will now compile and link assets properly. Set the mongo url to use environmental variables. Datastores on prod and dev now both use MongoDB.

--- tasks/register/buildProd.js
+++ tasks/register/buildProd.js
@@ -27,4 +27,3 @@ module.exports = function(grunt) {
     'clean:afterBuildProd'
   ]);
 };
-
--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -13,15 +13,15 @@
  */
 module.exports = function(grunt) {
   grunt.registerTask('prod', [
-    'polyfill:prod', //« Remove this to skip transpilation in production (not recommended)
+    // 'polyfill:prod', //« Remove this to skip transpilation in production (not recommended)
     'compileAssets',
-    'babel',         //« Remove this to skip transpilation in production (not recommended)
-    'concat',
-    'uglify',
-    'cssmin',
-    'sails-linker:prodJs',
-    'sails-linker:prodStyles',
-    'sails-linker:clientSideTemplates',
+    // 'babel',         //« Remove this to skip transpilation in production (not recommended)
+    // 'concat',
+    // 'uglify',
+    // 'cssmin',
+    // 'sails-linker:prodJs',
+    // 'sails-linker:prodStyles',
+    // 'sails-linker:clientSideTemplates',
+		'linkAssets',
   ]);
 };
-
