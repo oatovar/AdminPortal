4199e806c10bef5a11842740c14649aecd18b010
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2018-11-26 16:22:20 -0600

    Dashboard controller now passes the links for the user's role.

--- api/controllers/dashboard.js
+++ api/controllers/dashboard.js
@@ -1,6 +1,12 @@
 module.exports = async function showDashboard (req, res) {
   if (req.session.isAuthenticated) {
-    return res.view('pages/dashboard', {user: req.session.user});
+    // Grab links that are assigned to the that user's role.
+    let group = await Links.findOne({ 'Group': req.session.user.role });
+
+    for (let i in group.Links) {
+      console.log(i + ': ' + group.Links[i]);
+    }
+    return res.view('pages/dashboard', {user: req.session.user, links: group.Links});
   } else {
     return res.redirect('/login');
   }
