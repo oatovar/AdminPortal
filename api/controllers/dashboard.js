b59afc88eef0313a8833a8683d486b9c226a1ec8
Chris Hill <chris.hill@chriss-mbp.lan>
2018-11-26 17:01:05 -0600

    links populate with db data

--- api/controllers/dashboard.js
+++ api/controllers/dashboard.js
@@ -2,11 +2,17 @@ module.exports = async function showDashboard (req, res) {
   if (req.session.isAuthenticated) {
     // Grab links that are assigned to the that user's role.
     let group = await Links.findOne({ 'Group': req.session.user.role });
+    let global = await Links.findOne({ 'Group': 'Global'});
 
     for (let i in group.Links) {
       console.log(i + ': ' + group.Links[i]);
     }
-    return res.view('pages/dashboard', {user: req.session.user, links: group.Links});
+
+    for (let i in global.Links) {
+      console.log(i + ': ' + global.Links[i]);
+    }
+
+    return res.view('pages/dashboard', {user: req.session.user, groupLinks: group.Links, globalLinks: global.Links});
   } else {
     return res.redirect('/login');
   }
