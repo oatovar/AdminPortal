54d1f9e40d62163449565cc2a8c16965395fbe31
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2018-11-26 16:02:12 -0600

    Created model for the links that are held in the db collection for each admin role.

--- api/controllers/test.js
+++ api/controllers/test.js
@@ -1,17 +1,22 @@
 module.exports = async function action (req, res) {
-  let user = await User.findOne({username: 'john.doe'});
+  // let user = await User.findOne({username: 'john.doe'});
+  //
+  // if (!req.session.userId) {
+  //   console.log(user.firstName);
+  //   console.log(user.lastName);
+  //   console.log(user.username);
+  //   console.log(user.password);
+  //   req.session.userId = user.username;
+  //   req.session.isAuthenticated = true;
+  //   return res.json(user);
+  // } else {
+  //   console.log('Session already stored.');
+  //   return res.status(420).send('You\'re already authenticated!');
+  // }
 
-  if (!req.session.userId) {
-    console.log(user.firstName);
-    console.log(user.lastName);
-    console.log(user.username);
-    console.log(user.password);
-    req.session.userId = user.username;
-    req.session.isAuthenticated = true;
-    return res.json(user);
-  } else {
-    console.log('Session already stored.');
-    return res.status(420).send('You\'re already authenticated!');
-  }
+  let links = await Links.find({ });
 
+  console.log(links);
+
+  res.ok();
 };
---
+++ api/models/Links.js
@@ -0,0 +1,29 @@
+/**
+ * Links.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    Group: { type: 'string', required: true },
+    Links: { type: 'json', required: true },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+  tableName: 'Links',
+};
