3b3cdae770758de39fc69fa4d8ccd8f07142d522
Chris Hill <chris.hill@chriss-mbp.lan>
2018-11-29 02:24:42 -0600

    Now only Super Admins can access the users Management page.

--- api/controllers/database/users.js
+++ api/controllers/database/users.js
@@ -15,6 +15,14 @@ module.exports = async function users(req, res) {
   let groups = await Links.find({}); // Find all links.
   // sails.log.debug(groups)
   // sails.log.debug('Super Admin requested it\'s dashboard');
-  return res.view('pages/superadmin', { userGroups: groups });
-
+  if(req.session.user == undefined){
+    return res.view('pages/entrance/login', {invalidLogin: true});
+  } 
+  else if(req.session.user.role == "Super Admin"){
+    return res.view('pages/superadmin', { userGroups: groups });
+  } 
+  else {
+    req.session.invalidLogin = false;
+    return res.redirect('dashboard');
+  }
 };
